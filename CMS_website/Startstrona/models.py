6246f1859017d832c6a4e8092f1cce43a6d76e5f
Z-iutek <ziutugra@gmail.com>
2024-04-14 15:27:27 +0200

    Finalizacja projektu

--- CMS_website/Startstrona/models.py
+++ CMS_website/Startstrona/models.py
@@ -6,12 +6,14 @@ class Users(models.Model):
 
 class StanStrony(models.Model):
 
-  kolor_tla = models.CharField(max_length=7, default='Wpisz kolor')
+  kolor_tla = models.CharField(max_length=12, default='Wpisz kolor')
   tytul_strony = models.CharField(max_length=200, default='Wybierz tytuł')
   nazwa = models.CharField(max_length=100, default='Nazwa zapisu')  # Unikalna nazwa dla każdego stanu
   Naglowek = models.CharField(max_length=200, default='Wpisz naglowek')
   Tekst_glowny = models.TextField(max_length=2000, default='Wpisz tekst glowny')
   Stopka = models.CharField(max_length=200, default='Wpisz stopke')
+  Data = models.CharField(max_length=10, default='YEAR-MM-DD')
+
 
 
   def __str__(self):
